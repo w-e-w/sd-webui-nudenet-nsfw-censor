1834695f6d6df9ee6a99d517ae8e8f7f122c4fe2
w-e-w <40751091+w-e-w@users.noreply.github.com>
2025-06-11 18:22:44 +0900

    add Entire image (always) mask shape for Extras tab

--- scripts/nudenet_nsfw_censor_scripts/post_processing_script.py
+++ scripts/nudenet_nsfw_censor_scripts/post_processing_script.py
@@ -48,12 +48,15 @@ filter_opt_ui_show_dict = {
     'Fill color': [False, False, False, True, True, False],
     'No censor': [False, False, False, False, True, False],
 }
+
+entire_image_always = 'Entire image (always)'
 mask_shape_opt_ui_show_dict = {
     # [(mask_blend_radius, mask_blend_radius_variable_blur), rectangle_round_radius, nms_threshold]
     'Ellipse': [True, False, True],
     'Rectangle': [True, False, True],
     'Rounded rectangle': [True, True, True],
-    'Entire image': [False, False, False]
+    'Entire image': [False, False, False],
+    entire_image_always: [False, False, False],
 }
 
 
@@ -75,8 +78,8 @@ class ScriptPostprocessingNudenetCensor(scripts_postprocessing.ScriptPostprocess
                     save_mask = gr.Checkbox(False, label='Save mask')
                     override_settings = gr.Checkbox(False, label='Override filter configs')
                 with gr.Row() as row_1:
-                    mask_shape = gr.Dropdown(value='Ellipse', choices=list(mask_shapes_func_dict), label='Mask shape', visible=False)
                     filter_type = gr.Dropdown(value='Variable blur', label='Censor filter', choices=list(filter_opt_ui_show_dict), visible=False)
+                    mask_shape = gr.Dropdown(value='Ellipse', choices=list(mask_shapes_func_dict) + [entire_image_always], label='Mask shape', visible=False)
                 with gr.Row() as row_2:
                     blur_radius = gr.Slider(0, 100, 10, label='Blur radius', visible=False)  # Variable blur Gaussian Blur
                     blur_strength_curve = gr.Slider(0, 6, 3, label='Blur strength curve', visible=False)  # Variable blur
@@ -160,7 +163,7 @@ class ScriptPostprocessingNudenetCensor(scripts_postprocessing.ScriptPostprocess
                 def update_opt_ui(_filter_type, _mask_shape, _override_settings, _enable_nudenet):
                     filter_opt_enable_list = filter_opt_ui_show_dict[_filter_type]
                     mask_shape_opt_show_list = mask_shape_opt_ui_show_dict[_mask_shape]
-                    # blur_radius, blur_strength_curve, pixelation_factor, fill_color, mask_blend_radius, mask_blend_radius_variable_blur, rectangle_round_radius, nms_threshold, row_1, row_2
+
                     return (
                         gr.Dropdown.update(visible=_override_settings),  # filter_type
                         gr.Dropdown.update(visible=_override_settings),  # mask_shape
@@ -170,10 +173,10 @@ class ScriptPostprocessingNudenetCensor(scripts_postprocessing.ScriptPostprocess
                         gr.ColorPicker.update(visible=_override_settings and filter_opt_enable_list[3]),  # fill_color
                         gr.Slider.update(visible=_override_settings and filter_opt_enable_list[4] and mask_shape_opt_show_list[0]),  # mask_blend_radius
                         gr.Slider.update(visible=_override_settings and filter_opt_enable_list[5] and mask_shape_opt_show_list[0]),  # mask_blend_radius_variable_blur
-                        gr.Number().update(visible=_override_settings and mask_shape_opt_show_list[1]),  # rectangle_round_radius
-                        gr.Slider.update(visible=_override_settings and mask_shape_opt_show_list[2] and _enable_nudenet),  # nms_threshold
+                        gr.Number().update(visible=_override_settings and _enable_nudenet and mask_shape_opt_show_list[1]),  # rectangle_round_radius
+                        gr.Slider.update(visible=_override_settings and _enable_nudenet and mask_shape_opt_show_list[2]),  # nms_threshold
                         gr.Row.update(visible=_override_settings),  # row_1
-                        gr.Row.update(visible=_override_settings and any(mask_shape_opt_show_list)),  # row_2
+                        gr.Row.update(visible=_override_settings or any(mask_shape_opt_show_list)),  # row_2
                     )
 
                 update_opt_ui_inputs = [
@@ -234,47 +237,50 @@ class ScriptPostprocessingNudenetCensor(scripts_postprocessing.ScriptPostprocess
             return
         censor_mask = None
 
-        if forge:
-            forge_canvas_bg = args.get('forge_canvas_bg')
-            forge_canvas_fg = args.get('forge_canvas_fg')
-            if args.get('upload_mask') and forge_canvas_bg:
-                censor_mask = forge_canvas_bg.convert('L').resize(pp.image.size)
-            if args.get('draw_mask') and forge_canvas_fg:
-                censor_mask = Image.new('L', pp.image.size, 0) if censor_mask is None else censor_mask
-                r, g, b, a = forge_canvas_fg.split()
-                draw_mask = a.convert('L').resize(pp.image.size)
-                censor_mask.paste(draw_mask, draw_mask)
+        if args['mask_shape'] == entire_image_always:
+            censor_mask = Image.new('L', pp.image.size, 255)
         else:
-            input_mask = args.get('input_mask')
-            if input_mask:
-                mask_source = args.get('mask_source')
-                if 'Upload mask' in mask_source:
-                    censor_mask = input_mask['image'].convert('L').resize(pp.image.size)
-                if 'Draw mask' in mask_source:
+            if forge:
+                forge_canvas_bg = args.get('forge_canvas_bg')
+                forge_canvas_fg = args.get('forge_canvas_fg')
+                if args.get('upload_mask') and forge_canvas_bg:
+                    censor_mask = forge_canvas_bg.convert('L').resize(pp.image.size)
+                if args.get('draw_mask') and forge_canvas_fg:
                     censor_mask = Image.new('L', pp.image.size, 0) if censor_mask is None else censor_mask
-                    draw_mask = input_mask['mask'].convert('L').resize(pp.image.size)
+                    r, g, b, a = forge_canvas_fg.split()
+                    draw_mask = a.convert('L').resize(pp.image.size)
                     censor_mask.paste(draw_mask, draw_mask)
-
-        if args['enable_nudenet']:
-            if args['override_settings']:
-                nms_threshold = args['nms_threshold']
-                mask_shape = args['mask_shape']
-                rectangle_round_radius = args['rectangle_round_radius']
             else:
-                nms_threshold = shared.opts.nudenet_nsfw_censor_nms_threshold
-                mask_shape = shared.opts.nudenet_nsfw_censor_mask_shape
-                rectangle_round_radius = shared.opts.nudenet_nsfw_censor_rectangle_round_radius
+                input_mask = args.get('input_mask')
+                if input_mask:
+                    mask_source = args.get('mask_source')
+                    if 'Upload mask' in mask_source:
+                        censor_mask = input_mask['image'].convert('L').resize(pp.image.size)
+                    if 'Draw mask' in mask_source:
+                        censor_mask = Image.new('L', pp.image.size, 0) if censor_mask is None else censor_mask
+                        draw_mask = input_mask['mask'].convert('L').resize(pp.image.size)
+                        censor_mask.paste(draw_mask, draw_mask)
 
-            if pil_nude_detector.thresholds is None:
-                pil_nude_detector.refresh_label_configs()
-            nudenet_mask = pil_nude_detector.get_censor_mask(pp.image, nms_threshold, mask_shape, rectangle_round_radius, pil_nude_detector.thresholds, pil_nude_detector.expand_horizontal, pil_nude_detector.expand_vertical)
-            if nudenet_mask is not None:
-                nudenet_mask = nudenet_mask.convert('L')
+            if args['enable_nudenet']:
+                if args['override_settings']:
+                    nms_threshold = args['nms_threshold']
+                    mask_shape = args['mask_shape']
+                    rectangle_round_radius = args['rectangle_round_radius']
+                else:
+                    nms_threshold = shared.opts.nudenet_nsfw_censor_nms_threshold
+                    mask_shape = shared.opts.nudenet_nsfw_censor_mask_shape
+                    rectangle_round_radius = shared.opts.nudenet_nsfw_censor_rectangle_round_radius
 
-            if nudenet_mask and censor_mask:
-                censor_mask.paste(nudenet_mask, nudenet_mask)
-            elif not censor_mask:
-                censor_mask = nudenet_mask
+                if pil_nude_detector.thresholds is None:
+                    pil_nude_detector.refresh_label_configs()
+                nudenet_mask = pil_nude_detector.get_censor_mask(pp.image, nms_threshold, mask_shape, rectangle_round_radius, pil_nude_detector.thresholds, pil_nude_detector.expand_horizontal, pil_nude_detector.expand_vertical)
+                if nudenet_mask is not None:
+                    nudenet_mask = nudenet_mask.convert('L')
+
+                if nudenet_mask and censor_mask:
+                    censor_mask.paste(nudenet_mask, nudenet_mask)
+                elif not censor_mask:
+                    censor_mask = nudenet_mask
 
         if censor_mask:
 
